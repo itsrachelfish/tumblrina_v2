99a562b29122ae9fa1ef37a9447202a5a58710af
dbladez <d3generative@gmail.com>
2014-10-24 03:48:17 -0400

    Commiting changes (v;

--- config/config.example.js
+++ config/config.example.js
@@ -1,3 +1,23 @@
 /**
  * Created by Daniel on 10/24/2014.
  */
+
+module.exports = {
+    connection: {
+        server: 'irc.freenode.net',
+        userName: 'tumblrina_v2',
+        port: 6667,
+        debug: true,
+        showErrors: true,
+        autoRejoin: true,
+        autoConnect: true,
+        channels: ['##dbuttz2'],
+        secure: false,
+        selfSigned: true,
+        certExpired: true,
+        floodProtection: false,
+        floodProtectionDelay: 1000,
+        antiGhost: false,
+        antiGhostPassword: ''
+    }
+}
--- modules/tumblr.js
+++ modules/tumblr.js
@@ -1,3 +1,55 @@
 /**
- * Created by Daniel on 10/24/2014.
+ * Created by Daniel on 10/23/2014.
  */
+
+var tumblr = {
+    name: 'Tumblr',
+    client: null,
+    core: null,
+    commands: ['tu'],
+
+    tu: function(from, to, message) {
+        tumblr.client.say(to, 'heheheh');
+    },
+
+    onMessage: function(from, to, text, rawMessage) {
+        tumblr.core.log('DEBUG', tumblr.name + ' - ' + from + ' -> ' + to + ': ' + text);
+        var parsed = tumblr.core.parseCommand(text);
+        tumblr.core.log('DEBUG', 'Command ' + parsed.command + ' -> ' + parsed.message);
+        if(tumblr.commands.indexOf(parsed.command) > -1) {
+            tumblr[parsed.command](from, to, parsed.message);
+        }
+    },
+
+    load: function() {
+        if(tumblr.core === null) {
+            console.log('[ERROR] ' + tumblr.name + ' module cannot load CORE');
+            return;
+        }
+        if(tumblr.client === null) {
+            console.log('[ERROR] ' + tumblr.name + ' module cannot load CLIENT');
+            return;
+        }
+
+        tumblr.core.log('INFO', 'Loaded ' + tumblr.name + ' module');
+        tumblr.client.on('message', tumblr.onMessage);
+    },
+
+    unload: function() {
+        tumblr.core.log('INFO', 'Unloaded ' + tumblr.name + ' module');
+        tumblr.client.removeListener('message', tumblr.onMessage);
+    }
+}
+
+module.exports = {
+    load: function(client, core) {
+        tumblr.core = core;
+        tumblr.client = client;
+        tumblr.load();
+    },
+
+    unload: function() {
+        tumblr.unload();
+        delete tumblr;
+    }
+}
